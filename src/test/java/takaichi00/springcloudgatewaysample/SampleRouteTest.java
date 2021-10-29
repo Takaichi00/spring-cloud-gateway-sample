1e3993974a10a6b826c041210b0c37c30c2ff27f
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2021-10-29 23:35:46 +0900

    refactor: test を切り分け & 修正

--- src/test/java/takaichi00/springcloudgatewaysample/SampleRouteTest.java
+++ src/test/java/takaichi00/springcloudgatewaysample/SampleRouteTest.java
@@ -12,23 +12,17 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {"http_bin=http://localhost:${wiremock.server.port}"})
 @AutoConfigureWireMock(port = 0)
-class SpringCloudGatewaySampleApplicationTests {
+class SampleRouteTest {
 
   @Autowired
   private WebTestClient webClient;
 
   @Test
-  void contextLoads() {
-    //Stubs
+  void getHttpBin() {
     stubFor(get(urlEqualTo("/get"))
         .willReturn(aResponse()
             .withBody("{\"headers\":{\"Hello\":\"World\"}}")
             .withHeader("Content-Type", "application/json")));
-
-    stubFor(get(urlEqualTo("/delay/3"))
-        .willReturn(aResponse()
-            .withBody("no fallback")
-            .withFixedDelay(3000)));
 
     webClient
         .get().uri("/get")
@@ -36,6 +30,14 @@ class SpringCloudGatewaySampleApplicationTests {
         .expectStatus().isOk()
         .expectBody()
         .jsonPath("$.headers.Hello").isEqualTo("World");
+  }
+
+  @Test
+  void circuitbreaker() {
+    stubFor(get(urlEqualTo("/delay/3"))
+        .willReturn(aResponse()
+            .withBody("no fallback")
+            .withFixedDelay(3000)));
     webClient
         .get().uri("/delay/3")
         .header("Host", "www.circuitbreaker.com")
@@ -44,7 +46,6 @@ class SpringCloudGatewaySampleApplicationTests {
         .expectBody()
         .consumeWith(
             response -> assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
-
   }
 
   @Test
