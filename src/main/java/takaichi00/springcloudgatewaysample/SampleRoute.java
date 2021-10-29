e41329a9c25982dccba10034306d3ba48ff4cdff
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2021-10-30 00:02:31 +0900

    refactor: Route を切り出した

---
+++ src/main/java/takaichi00/springcloudgatewaysample/SampleRoute.java
@@ -0,0 +1,35 @@
+package takaichi00.springcloudgatewaysample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SampleRoute {
+
+  @Autowired
+  SampleFilter sampleFilter;
+
+  @Bean
+  public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+    String httpUri = uriConfiguration.getHttpbin();
+    return builder.routes()
+        .route(p -> p
+            .path("/get")
+            .filters(f -> f.addRequestHeader("Hello", "World"))
+            .uri(httpUri))
+        .route(p -> p
+            .path("/get/1").filters(f -> f.filter(sampleFilter))
+            .uri(httpUri))
+        .route(p -> p
+            .host("*.circuitbreaker.com")
+            .filters(f -> f
+                .circuitBreaker(config -> config
+                    .setName("mycmd")
+                    .setFallbackUri("forward:/fallback")))
+            .uri(httpUri))
+        .build();
+  }
+}
--- src/main/java/takaichi00/springcloudgatewaysample/SpringCloudGatewaySampleApplication.java
+++ src/main/java/takaichi00/springcloudgatewaysample/SpringCloudGatewaySampleApplication.java
@@ -1,42 +1,13 @@
 package takaichi00.springcloudgatewaysample;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(UriConfiguration.class)
 public class SpringCloudGatewaySampleApplication {
-
-  @Autowired
-  SampleFilter sampleFilter;
-
   public static void main(String[] args) {
     SpringApplication.run(SpringCloudGatewaySampleApplication.class, args);
-  }
-
-  @Bean
-  public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
-    String httpUri = uriConfiguration.getHttpbin();
-    return builder.routes()
-        .route(p -> p
-            .path("/get")
-            .filters(f -> f.addRequestHeader("Hello", "World"))
-            .uri(httpUri))
-        .route(p -> p
-            .path("/get/1").filters(f -> f.filter(sampleFilter))
-            .uri(httpUri))
-        .route(p -> p
-            .host("*.circuitbreaker.com")
-            .filters(f -> f
-                .circuitBreaker(config -> config
-                    .setName("mycmd")
-                    .setFallbackUri("forward:/fallback")))
-            .uri(httpUri))
-        .build();
   }
 }
