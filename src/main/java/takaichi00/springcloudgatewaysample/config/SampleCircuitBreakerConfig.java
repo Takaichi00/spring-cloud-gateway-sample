13df5ca7d7b620bb9a16a6244881c63ce054e098
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2021-11-15 23:22:13 +0900

    feat: circuit breaker のデフォルトタイムアウト値を設定

---
+++ src/main/java/takaichi00/springcloudgatewaysample/config/SampleCircuitBreakerConfig.java
@@ -0,0 +1,23 @@
+package takaichi00.springcloudgatewaysample.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import java.time.Duration;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SampleCircuitBreakerConfig {
+  @Bean
+  public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+    return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+        .circuitBreakerConfig(CircuitBreakerConfig.custom().build())
+        .timeLimiterConfig(TimeLimiterConfig.custom()
+            .timeoutDuration(Duration.ofMillis(10000))
+            .build())
+        .build());
+  }
+}
